f918ebd57e0d1b81b8d03e028deca23a1e17e705
danny <dannysladkiy@yandex.ru>
2023-08-04 16:08:31 +0300

    fix style

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -10,6 +10,27 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
+    @Query(value = "SELECT b FROM Booking b WHERE item.id = :itemId AND status <> 'REJECTED' AND NOW() BETWEEN b.start AND b.end")
+    Booking findCurrentByItem(Long itemId);
+
+    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.id = :itemId AND EXTRACT(epoch FROM b2.start - NOW()) > 0 AND b2.status <> 'REJECTED' order by EXTRACT(epoch FROM b2.start - NOW()))")
+    List<Booking> findNextByItem(Long itemId);
+
+    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.id = :itemId AND EXTRACT(epoch FROM b2.start - NOW()) <= 0 ORDER BY EXTRACT(epoch FROM b2.start - NOW()) desc)")
+    List<Booking> findLastByItem(Long itemId);
+
+    @Query(value = "select b from Booking b where b.item.id = :itemId and b.booker.id = :userId order by b.end")
+    List<Booking> findAllByItemAndUser(Long itemId, Long userId);
+
+    @Query(value = "select b from Booking b where b.booker.id = :userId order by b.end")
+    List<Booking> findCurrentsByUser(Long userId);
+
+    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.owner.id = :userId AND EXTRACT(epoch FROM b2.start - NOW()) > 0 AND b2.status <> 'REJECTED' order by EXTRACT(epoch FROM b2.start - NOW()))")
+    List<Booking> findNextsByUser(Long userId);
+
+    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.owner.id = :userId AND EXTRACT(epoch FROM b2.start - NOW()) <= 0 ORDER BY EXTRACT(epoch FROM b2.start - NOW()) desc)")
+    List<Booking> findLastsByUser(Long userId);
+
     List<Booking> findAllByItem_Owner_IdOrderByEndDesc(Long id);
 
     List<Booking> findAllByItem_Owner_IdAndStartBeforeAndEndAfter(Long ownerId, LocalDateTime start, LocalDateTime end);
@@ -29,26 +50,6 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findAllByBooker_IdAndStatusIs(Long ownerId, BookingStatus status);
 
     List<Booking> findAllByBooker_IdOrderByEndDesc(Long id);
-    
-    @Query(value = "SELECT b FROM Booking b WHERE item.id = :itemId AND status <> 'REJECTED' AND NOW() BETWEEN b.start AND b.end")
-    Booking findCurrentByItem(Long itemId);
-
-    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.id = :itemId AND EXTRACT(epoch FROM b2.start - NOW()) > 0 AND b2.status <> 'REJECTED' order by EXTRACT(epoch FROM b2.start - NOW()))")
-    List<Booking> findNextByItem(Long itemId);
-
-    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.id = :itemId AND EXTRACT(epoch FROM b2.start - NOW()) <= 0 ORDER BY EXTRACT(epoch FROM b2.start - NOW()) desc)")
-    List<Booking> findLastByItem(Long itemId);
-    @Query(value = "select b from Booking b where b.item.id = :itemId and b.booker.id = :userId order by b.end")
-    List<Booking> findAllByItemAndUser(Long itemId, Long userId);
-
-    @Query(value = "select b from Booking b where b.booker.id = :userId order by b.end")
-    List<Booking> findCurrentsByUser(Long userId);
-
-    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.owner.id = :userId AND EXTRACT(epoch FROM b2.start - NOW()) > 0 AND b2.status <> 'REJECTED' order by EXTRACT(epoch FROM b2.start - NOW()))")
-    List<Booking> findNextsByUser(Long userId);
-
-    @Query(value = "SELECT b FROM Booking b WHERE b.id IN (SELECT b2.id FROM Booking b2 WHERE b2.item.owner.id = :userId AND EXTRACT(epoch FROM b2.start - NOW()) <= 0 ORDER BY EXTRACT(epoch FROM b2.start - NOW()) desc)")
-    List<Booking> findLastsByUser(Long userId);
 
     Booking findFirstByItem_IdAndStartBeforeAndEndAfter(Long itemId, LocalDateTime start, LocalDateTime end);
 }
